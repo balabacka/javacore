fac246678e9a60d07d2df4a24a7776806c512e1f
balabacka <pavelartiukh@gmail.com>
2017-01-25 18:47:22 +0200

    structure

---
+++ src/main/java/com/balabacka/app/classwork/lesson03/MathFunc.java
@@ -0,0 +1,10 @@
+package com.balabacka.app.classwork.lesson03;
+
+/**
+ * Created by pc on 25.01.2017.
+ */
+public class MathFunc {
+    public static int multiply(int a, int b){
+        return a * b;
+    }
+}
---
+++ src/test/java/com/balabacka/app/classwork/lesson03/MathFuncTest.java
@@ -0,0 +1,16 @@
+package com.balabacka.app.classwork.lesson03;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by pc on 25.01.2017.
+ */
+public class MathFuncTest {
+    @Test
+    public void multiply() throws Exception {
+
+    }
+
+}
