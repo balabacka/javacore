87a7853808768c82bd05717d75e15877281e5417
balabacka <pavelartiukh@gmail.com>
2017-01-27 16:45:36 +0200

    test added

---
+++ src/main/java/com/balabacka/app/classwork/lesson05/Calculators.java
@@ -0,0 +1,13 @@
+package com.balabacka.app.classwork.lesson05;
+
+import static com.balabacka.app.classwork.lesson05.CircleRadius.*;
+
+/**
+ * Created by pc on 27.01.2017.
+ */
+public class Calculators {
+    public static void main( String[] args ){
+
+    calculateBigger();
+    }
+}
